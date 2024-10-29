5103273efb624a2901312ba44e485ddd54a2a32e
Vilma Kainulainen <2306052@edu.karelia.fi>
2024-10-29 17:00:52 +0200

    Add project files.

---
+++ App/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AppLogger;
+
+namespace App;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Logger.Log("kissa_miumau");
+    }
+}
+
+
---
+++ AppLogger/AppLogger.cs
@@ -0,0 +1,12 @@
+﻿using Humanizer;
+using System;
+namespace AppLogger
+{
+    public class Logger
+    {
+        public static void Log(string text)
+        {
+            Console.WriteLine(text.Humanize());
+        }
+    }
+}
